cbc130e404d1f825824de6dd6ffcb2c24514a574
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-03-19 08:22:59 -0300

    Inserindo contatos na agenda

--- config/process.php
+++ config/process.php
@@ -3,28 +3,59 @@ session_start();
 
 include_once ("connection.php");
 include_once ("url.php");
-$id;
 
-if (!empty ($_GET)) {
-    $id = $_GET["id"];
-}
+$data = $_POST;
+if (!empty ($data)) {
 
-if (!empty ($id)) {
+    if ($data["type"] === "create") {
+        $name = $data["name"];
+        $phone = $data["phone"];
+        $observations = $data["observations"];
+        $query = "INSERT INTO contacts (name, phone, observations) VALUES(:name, :phone, :observations)";
 
-    $query = "SELECT * FROM contacts WHERE id = :id ";
-    $stmt = $conn-> prepare($query);
-    $stmt-> bindParam(":id" , $id);
-    $stmt-> execute();
-    $contacts = $stmt-> fetch();
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":phone", $phone);
+        $stmt->bindParam(":observations", $observations);
+
+        try {
+
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato criado com sucesso!";
+
+        } catch (PDOException $e) {
+            $error = $e->getMessage();
+            echo "Erro:  $error";
+        }
+    }
+    header("Location:" . $BASE_URL . "../index.php");
+
 } else {
-    $contacts = [];
-    $query = "SELECT * FROM contacts";
+    $id;
+
+    if (!empty ($_GET)) {
+        $id = $_GET["id"];
+    }
+
+    if (!empty ($id)) {
+
+        $query = "SELECT * FROM contacts WHERE id = :id ";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $contacts = $stmt->fetch();
+    } else {
+        $contacts = [];
+        $query = "SELECT * FROM contacts";
 
 
-    $stmt = $conn->prepare($query);
+        $stmt = $conn->prepare($query);
 
-    $stmt->execute();
-    $contacts = $stmt->fetchAll();
+        $stmt->execute();
+        $contacts = $stmt->fetchAll();
+    }
+
 }
 
-
+$conn = null;
