823e51c0c97e85a57d94b3d526aa6cea41bc001d
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-03-22 10:48:36 -0300

    Removendo contatos

--- config/process.php
+++ config/process.php
@@ -28,31 +28,47 @@ if (!empty ($data)) {
             $error = $e->getMessage();
             echo "Erro:  $error";
         }
-    }else if($data["type"] === "edit"){
+    } else if ($data["type"] === "edit") {
         $name = $data["name"];
         $phone = $data["phone"];
         $observations = $data["observations"];
         $id = $data["id"];
-        
+
         $query = "UPDATE contacts 
                   SET name = :name, phone = :phone, observations = :observations 
                    WHERE id = :id";
 
-                   $stmt = $conn->prepare($query);
-                   $stmt-> bindParam(":name", $name);
-                   $stmt-> bindParam(":phone", $phone);
-                   $stmt-> bindParam(":observations", $observations);
-                   $stmt-> bindParam(":id", $id);
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":phone", $phone);
+        $stmt->bindParam(":observations", $observations);
+        $stmt->bindParam(":id", $id);
 
-                   try {
+        try {
 
-                    $stmt->execute();
-                    $_SESSION["msg"] = "Contato atualizado com sucesso!";
-        
-                } catch (PDOException $e) {
-                    $error = $e->getMessage();
-                    echo "Erro:  $error";
-                }
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato atualizado com sucesso!";
+
+        } catch (PDOException $e) {
+            $error = $e->getMessage();
+            echo "Erro:  $error";
+        }
+
+    } else if ($data["type"] === "delete") {
+        $id = $data["id"];
+        $query = "DELETE FROM contacts WHERE id =:id";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+
+
+        try {
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato removido com sucesso!";
+
+        } catch (PDOException $e) {
+            $error = $e->getMessage();
+            echo "Erro:  $error";
+        }
 
     }
     header("Location:" . $BASE_URL . "../index.php");
--- index.php
+++ index.php
@@ -9,30 +9,43 @@ include_once ("templates/header.php");
     <?php endif; ?>
     <h1 id="main-title">Minha Agenda</h1>
     <?php if (count($contacts) > 0): ?>
-       <table class="table" id="contacts-table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Telefone</th>
-                <th scope="col"></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php  foreach($contacts as $contacts): ?>
+        <table class="table" id="contacts-table">
+            <thead>
                 <tr>
-                    <td scope="row" class="col-id"><?= $contacts["id"] ?></td>
-                    <td scope="row"><?= $contacts["name"] ?></td>
-                    <td scope="row"><?= $contacts["phone"] ?></td>
-                    <td class="actions">
-                        <a href="<?= $BASE_URL ?>show.php?id=<?= $contacts["id"] ?>"><i class="fas fa-eye chack-icon"></i></a>
-                        <a href="<?= $BASE_URL ?>edit.php?id=<?= $contacts["id"] ?>"><i class="far fa-edit edit-icon"></i></a>
-                        <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
-                    </td>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col"></th>
                 </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($contacts as $contacts): ?>
+                    <tr>
+                        <td scope="row" class="col-id">
+                            <?= $contacts["id"] ?>
+                        </td>
+                        <td scope="row">
+                            <?= $contacts["name"] ?>
+                        </td>
+                        <td scope="row">
+                            <?= $contacts["phone"] ?>
+                        </td>
+                        <td class="actions">
+                            <a href="<?= $BASE_URL ?>show.php?id=<?= $contacts["id"] ?>"><i
+                                    class="fas fa-eye chack-icon"></i></a>
+                            <a href="<?= $BASE_URL ?>edit.php?id=<?= $contacts["id"] ?>"><i
+                                    class="far fa-edit edit-icon"></i></a>
+                            <form class="delete-form" action="<?= $BASE_URL ?>/config/process.php" method="POST">
+                                <input type="hidden" name="type" value="delete">
+                                <input type="hidden" name="id" value="<?=$contact["id"]?>">
+                                <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
+                            </form>
+
+                        </td>
+                    </tr>
                 <?php endforeach; ?>
-        </tbody>
-       </table>
+            </tbody>
+        </table>
     <?php else: ?>
         <p id="emply-list-text">Ainda não há contatos na sua agenda <a href="<?= $BASE_URL ?>create.php"> Clique aqui para
                 adicionar.</a></p>
